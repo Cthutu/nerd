6cc97c93cba0a93e563c02d66b3ae54c0c9afed2
Matt Davies <mattie.davies@gmail.com>
2026-02-17 20:28:00 +0000

    feat(compiler): define public interface and data structures

--- src/compiler/compiler.h
+++ src/compiler/compiler.h
@@ -3,10 +3,31 @@
 //
 // Copyright (C)2026 Matt Davies, all rights reserved
 //------------------------------------------------------------------------------
-//> use: lexer
+//> use: lexer timing
 
 #pragma once
 
 #include <compiler/lexer/lexer.h>
+#include <timing/timing.h>
+
+//------------------------------------------------------------------------------
+
+typedef struct {
+    Lexer lexer;
+} FrontEndResults;
+
+typedef struct {
+} BackEndResults;
+
+FrontEndResults front_end(string source_code, Timing* timing);
+void            front_end_results_done(FrontEndResults* results);
+
+BackEndResults back_end(const FrontEndResults* front_end_results,
+                        Timing*                timing);
+void           back_end_results_done(BackEndResults* results);
+
+void compiler_dump(bool                   dump_lexer,
+                   const FrontEndResults* front_end_results,
+                   const BackEndResults*  back_end_results);
 
 //------------------------------------------------------------------------------
