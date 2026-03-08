e21c5ad3fad998879128b2dee1a5e3ef04c23978
Matt Davies <mattie.davies@gmail.com>
2026-03-08 13:04:23 +0000

    Add timing to compiler

--- src/compiler/compiler.h
+++ src/compiler/compiler.h
@@ -3,7 +3,7 @@
 //
 // Copyright (C)2026 Matt Davies, all rights reserved
 //------------------------------------------------------------------------------
-//> use: lexer timing
+//> use: timing
 
 #pragma once
 
