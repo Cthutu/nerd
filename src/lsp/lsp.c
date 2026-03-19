ef7844033fd29f9d869901462886aba33403ed0f
Matt Davies <mattie.davies@gmail.com>
2026-03-19 09:31:17 +0000

    feat(lsp): Implement document content management

--- src/lsp/lsp.c
+++ src/lsp/lsp.c
@@ -279,35 +279,67 @@ void lsp_handle_initialise(LspState* state, const LspMessage* message)
     lsp_send_response(arena, response);
 }
 
-void lsp_handle_shutdown(const LspMessage* message)
+void lsp_handle_shutdown(LspState* state, const LspMessage* message)
 {
+    UNUSED(state);
+
     JsonValue* response = lsp_prepare_response(message);
     json_object_set_null(response, message->arena, "result");
     lsp_send_response(message->arena, response);
 }
 
-void lsp_handle_initialised(const LspMessage* message)
+void lsp_handle_initialised(LspState* state, const LspMessage* message)
 {
+    UNUSED(state);
     UNUSED(message);
     lsp_log("Client initialised");
 }
 
-void lsp_handle_did_open(const LspMessage* message)
+void lsp_handle_did_open(LspState* state, const LspMessage* message)
 {
     JsonValue* uri = json_get_cstr(message->message, "params.textDocument.uri");
+    JsonValue* text =
+        json_get_cstr(message->message, "params.textDocument.text");
     if (uri && uri->kind == JSON_STRING) {
         lsp_log("Opened " STRINGP, STRINGV(json_string(uri)));
     } else {
-        lsp_log("Opened document");
+        return;
+    }
+
+    if (text && text->kind == JSON_STRING) {
+        string text_str  = json_string(text);
+
+        bool   new_arena = false;
+        Arena* arena =
+            LspDocumentMap_entry(&state->documents, text_str, &new_arena);
+        if (new_arena) {
+            arena_init(arena);
+        } else {
+            arena_reset(arena);
+        }
+
+        u8*    document_copy = (u8*)arena_alloc(arena, text_str.count);
+        string src_document  = json_string(text);
+        memcpy(document_copy, src_document.data, src_document.count);
+
+        lsp_log("Document content:\n" STRINGP, STRINGV(json_string(text)));
     }
 }
 
-void lsp_handle_did_close(const LspMessage* message)
+void lsp_handle_did_close(LspState* state, const LspMessage* message)
 {
     JsonValue* uri = json_get_cstr(message->message, "params.textDocument.uri");
+    JsonValue* text =
+        json_get_cstr(message->message, "params.textDocument.text");
+
     if (uri && uri->kind == JSON_STRING) {
         lsp_log("Closed " STRINGP, STRINGV(json_string(uri)));
     } else {
-        lsp_log("Closed document");
+        return;
+    }
+
+    if (text && text->kind == JSON_STRING) {
+        string text_str = json_string(text);
+        LspDocumentMap_delete(&state->documents, text_str);
     }
 }
