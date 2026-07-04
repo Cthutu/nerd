764baae03dbfc59f441cc2f5360183310d01a07a
Matt Davies <mattie.davies@gmail.com>
2026-07-04 19:27:45 +0100

    Add missing OpenGL texture enums

---
+++ tests/commands/230-check-std-opengl-texture-enums.cmd
@@ -0,0 +1,29 @@
+use std.opengl
+
+main :: fn () {
+    _wrap_s : GLenum = GL_TEXTURE_WRAP_S
+    _wrap_t : GLenum = GL_TEXTURE_WRAP_T
+    _wrap_r : GLenum = GL_TEXTURE_WRAP_R
+
+    _nearest : GLenum = GL_NEAREST
+    _linear : GLenum = GL_LINEAR
+    _nearest_mipmap_nearest : GLenum = GL_NEAREST_MIPMAP_NEAREST
+    _linear_mipmap_nearest : GLenum = GL_LINEAR_MIPMAP_NEAREST
+    _nearest_mipmap_linear : GLenum = GL_NEAREST_MIPMAP_LINEAR
+    _linear_mipmap_linear : GLenum = GL_LINEAR_MIPMAP_LINEAR
+
+    _repeat : GLenum = GL_REPEAT
+    _clamp_to_border : GLenum = GL_CLAMP_TO_BORDER
+    _clamp_to_edge : GLenum = GL_CLAMP_TO_EDGE
+    _mirrored_repeat : GLenum = GL_MIRRORED_REPEAT
+}
+¬
+0
+¬
+
+¬
+delete
+¬
+
+¬
+check
